296d44e89db7981500412d50db26074f35596dea
87 <dev@87flowers.com>
2025-12-18 19:57:45 +0100

    update tulip

--- hosts/tulip.nix
+++ hosts/tulip.nix
@@ -1,7 +1,15 @@
-{
+let
+  inherit (import ../npins) nixos-hardware;
+in {
   imports =
     [ ../modules/common.nix
       ../modules/pc.nix
+
+      "${nixos-hardware}/lenovo/thinkpad/e14"
+      "${nixos-hardware}/common/cpu/amd"
+      "${nixos-hardware}/common/gpu/amd"
+      "${nixos-hardware}/common/cpu/amd/pstate.nix"
+
     ];
 
   networking.hostName = "tulip";
