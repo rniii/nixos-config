f73921b13471e7f9432e32dabf4985ca9aafcfd3
87 <dev@87flowers.com>
2026-05-18 01:17:56 +0200

    update tulip config

--- hosts/tulip.nix
+++ hosts/tulip.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   imports =
     with import ../npins;
@@ -10,4 +11,32 @@
     ];
 
   networking.hostName = "tulip";
+
+  services.tailscale.extraSetFlags =
+    [ "--accept-dns=false" "--accept-routes=false" ];
+
+  # nixos-generate-config --show-hardware-config
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/EFI";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = true;
 }
