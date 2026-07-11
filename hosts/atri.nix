d4b1f8fdab201ff681c90d128a936ee718b8f160
87 <dev@87flowers.com>
2026-07-11 20:01:44 +0200

    hw

--- hosts/atri.nix
+++ hosts/atri.nix
@@ -11,6 +11,27 @@
 
   hardware.firmware = [ pkgs.linux-firmware ];
 
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "amd_iommu=off"
+    "amd.gpu.gttsize=131072"
+    "ttm.pages_limit=31457280"
+  ];
+
+  boot.extraModprobeConfig = ''
+    options amdgpu gttsize=122800
+    options ttm pages_limit=31457280
+    # preallocate to reduce fragmentation (note: memory becomes permanently unavailable to rest of system)
+    options ttm page_pool_size=31457280
+  '';
+
+  environment.systemPackages = with pkgs;
+    [ tuned
+    ];
+
+  services.tuned.enable = true;
+
   # nixos-generate-config --show-hardware-config
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" "sdhci_pci" ];
@@ -36,4 +57,3 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = true;
 }
-
