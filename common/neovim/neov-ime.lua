06f4196c4fc4a35bdfd820beb225c51af809f69f
rini <nil@dissoc.cc>
2026-09-13 06:13:34 -0300

    UUUUUUUUUUUUUUu

--- common/neovim/neov-ime.lua
+++ common/neovim/neov-ime.lua
@@ -1,15 +1,21 @@
+local function set_ime(args)
+    vim.g.neovide_input_ime = args.event:match("Enter$") and true or false
+end
+
 local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
 
-vim.api.nvim_create_autocmd({"InsertEnter", "CmdlineEnter"}, {
+vim.api.nvim_create_autocmd({"InsertEnter", "InsertLeave"}, {
     group = ime_input,
     pattern = "*",
-    callback = function () vim.g.neovide_input_ime = true end
+    callback = set_ime
 })
 
-vim.api.nvim_create_autocmd({"InsertLeave", "CmdlineLeave"}, {
+vim.api.nvim_create_autocmd({"CmdlineEnter", "CmdlineLeave"}, {
     group = ime_input,
     pattern = "[/\\?]",
-    callback = function () vim.g.neovide_input_ime = false end
+    callback = set_ime
 })
+
+vim.g.neovide_input_ime = false
 
 -- vim: sw=4:
